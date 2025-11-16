25b8fc81115ec809f9ecde76c27a4b29836eb967
MatthewCarlAnderson <xkvt8mc3e3@snkmail.com>
2025-11-16 09:43:19 -0500

    Another slow one....

---
+++ leet-NumberOfSubstringsWithOnly1s-1513.py
@@ -0,0 +1,65 @@
+# Given a binary string s, return the number of substrings with all characters 1's. Since the answer may be too large, return it modulo 109 + 7.
+
+# Example 1:
+# Input: s = "0110111"
+# Output: 9
+# Explanation: There are 9 substring in total with only 1's characters.
+# "1" -> 5 times.
+# "11" -> 3 times.
+# "111" -> 1 time.
+
+# Example 2:
+# Input: s = "101"
+# Output: 2
+# Explanation: Substring "1" is shown 2 times in s.
+
+# Example 3:
+# Input: s = "111111"
+# Output: 21
+# Explanation: Each substring contains only 1's characters.
+
+# HINT: Count number of 1s in each consecutive-1 group.
+# For a group with n consecutive 1s, the total contribution of it to the final answer is
+# (n + 1) * n // 2.
+
+class Solution:
+    def numSub(self, s: str) -> int:
+        modulo = (10**9+7)
+        if len(s) == 0:
+            return 0
+        if len(s) == 1:
+            if s == "0":
+                return 0
+            return 1
+        
+        i: int = 0
+        currentCount: int = 0
+        retVal: int = 0
+        while i < len(s):
+            if s[i] == "0":
+                if currentCount > 0:
+                    retVal = (retVal + (((currentCount + 1) * currentCount) / 2)) % modulo
+                currentCount = 0
+            else:
+                currentCount +=1
+            i += 1
+        if currentCount > 0:
+            retVal = (retVal + (((currentCount + 1) * currentCount) / 2)) % modulo
+        
+        return int(retVal)
+    
+
+
+def main():
+    thingy = Solution()
+    print(thingy.numSub("111111")) # 21
+    print(thingy.numSub("111")) # 6
+    print(thingy.numSub("11")) # 3
+    print(thingy.numSub("1111")) # 10
+    print(thingy.numSub("101")) # 2
+    
+
+if __name__ == "__main__":
+    main()
+
+    
